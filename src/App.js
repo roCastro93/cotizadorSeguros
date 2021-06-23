cb4a37c33a3dc8dd38641c057dd7af529d67a612
Rodolfo <r.castromolina1993@gmail.com>
2021-06-22 22:18:44 -0400

    agregando formulario y styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Header from './components/Header'
+import Header from "./components/Header";
+import Formulario from './components/Formulario'
+import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`;
 function App() {
   return (
-    <Header 
-    titulo='Cotizador de seguros'
-    />
-
+    <Contenedor>
+      <Header titulo="Cotizador de seguros" />
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
