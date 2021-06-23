bd88954f86aa57f1d64e3431f73bb63c70fcff49
Rodolfo <r.castromolina1993@gmail.com>
2021-06-23 01:45:25 -0400

    cambios varios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,29 +1,86 @@
-import React from "react";
-import styled from '@emotion/styled';
-
-
+import React, { useState } from "react";
+import styled from "@emotion/styled";
 
 const Campo = styled.div`
-        display: flex;
-        margin-bottom: 1rem;
-        align-items: center;
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
 `;
 const Label = styled.label`
-    flex: 0 0 100px;
+  flex: 0 0 100px;
 `;
 const Select = styled.select`
-    display: block;
-    width: 100%;
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #ffff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 0.3s ease;
+  margin-top: 2rem;
+  &:hover {
+    background-color: #26c6da;
+    cursor: pointer;
+  }
+`;
+
+const Error =styled.div`
+    background-color: red;
+    color: white;
     padding: 1rem;
-    border: 1px solid #e1e1e1;
-    -webkit-appearance:none;
+    width: 100%;
+    text-align:center;
+    margin-bottom: 2rem;
 `;
 const Formulario = () => {
+  const [datos, setDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+  const [error, setError] = useState(false);
+
+  //extraer los valores del state
+  const { marca, year, plan } = datos;
+
+  const onChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (marca.trim() === "" || year.trim() === "" || plan.trim() === "") {
+      setTimeout(function(){
+      setError(true)
+      },3000)
+      return;
+      
+    }
+    setError(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
+      {error ?<Error>Todos los campos son obligatorios</Error> : null}
       <Campo>
         <Label>Marca</Label>
-        <Select name="" id="">
+        <Select name="marca" value={marca} onChange={onChange}>
           <option value="">-- Seleccione --</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
@@ -32,7 +89,7 @@ const Formulario = () => {
       </Campo>
       <Campo>
         <Label>Año</Label>
-        <Select name="" id="">
+        <Select name="year" value={year} onChange={onChange}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -48,12 +105,24 @@ const Formulario = () => {
       </Campo>
       <Campo>
         <Label htmlFor="">Plan</Label>
-        <input type="radio" name="plan" value="basico" />
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === "basico"}
+          onChange={onChange}
+        />
         Basico
-        <input type="radio" name="plan" value="completo" />
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === "completo"}
+          onChange={onChange}
+        />
         Completo
       </Campo>
-      <input type="button" value="Cotizar" />
+      <Boton type="submit">Cotizar</Boton>
     </form>
   );
 };
